ddd35c3294729c57a2fff756b2d2e6759338555c
Mattias Åslund <mattias@shoppa.com>
2025-09-26 10:33:13 +0200

    Switched to utf8

--- tests/AppConfigCli.Core.Tests/_TextTruncation.cs
+++ tests/AppConfigCli.Core.Tests/_TextTruncation.cs
@@ -7,8 +7,8 @@ public class _TextTruncation
     [Theory]
     [InlineData("hello", 10, "hello")]
     [InlineData("hello", 5, "hello")]
-    [InlineData("hello", 4, "hel…")]
-    [InlineData("abcdef", 1, "…")]
+    [InlineData("hello", 4, "helâ€¦")]
+    [InlineData("abcdef", 1, "â€¦")]
     [InlineData("abcdef", 0, "")]
     public void truncate_fixed_behaves_as_expected(string s, int width, string expected)
         => TextTruncation.TruncateFixed(s, width).Should().Be(expected);
@@ -17,6 +17,6 @@ public class _TextTruncation
     public void pad_column_truncates_and_pads_to_width()
     {
         TextTruncation.PadColumn("hello", 8).Should().Be("hello   ");
-        TextTruncation.PadColumn("longer-than", 5).Should().Be("long…");
+        TextTruncation.PadColumn("longer-than", 5).Should().Be("longâ€¦");
     }
 }
